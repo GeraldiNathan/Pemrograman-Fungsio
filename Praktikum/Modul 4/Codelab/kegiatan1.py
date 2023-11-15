2179739398049224809abaf587f658d0b3a8ead7
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2023-11-15 16:30:03 +0800

    Fix: Code in Hof and currying

--- Praktikum/Modul 4/Codelab/kegiatan1.py
+++ Praktikum/Modul 4/Codelab/kegiatan1.py
@@ -28,3 +28,29 @@
 # if __name__ == "__main__":
 #     main()
 
+
+
+# Kegiatan 1
+
+# curry function
+def perkalian(a):
+    def dengan(b):
+        return a*b
+    return dengan
+
+
+def main():
+# # HOF
+    hasil = map(perkalian(5), [1, 2, 3, 4, 5])
+    print(list(hasil))
+
+# # Curry Function
+#     hasil1 = perkalian(5)(1)
+#     hasil2 = perkalian(5)(2)
+#     hasil3 = perkalian(5)(3)
+#     hasil4 = perkalian(5)(4)
+#     hasil5 = perkalian(5)(5)
+#     print(hasil1, hasil2, hasil3, hasil4, hasil5)
+
+if __name__ == "__main__":
+    main()
