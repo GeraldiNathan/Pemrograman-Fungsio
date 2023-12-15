df6717ad4ecf2ee85a8a91fd211bd9572e68ddc3
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2023-12-15 15:40:19 +0800

    feat(Modul 6) : Percobaan 2 Done

---
+++ Praktikum/Modul 6/codelab/percobaan2.py
@@ -0,0 +1,13 @@
+from PIL import Image
+
+background_path = "C:\Kuliah\Semester 5\Fungsio\Praktikum\All Modules\Modules\Praktikum\Modul 6/assets\img\geral.jpg"
+overlay_path = "C:\Kuliah\Semester 5\Fungsio\Praktikum\All Modules\Modules\Praktikum\Modul 6/assets\img\osborn.jpg"
+background = Image.open(background_path)
+overlay = Image.open(overlay_path)
+overlay = overlay.convert("RGBA")
+overlay = overlay.resize((1200, 1200))
+x_position = 1000
+y_position = 850
+background.paste(overlay, (x_position, y_position), overlay)
+background.save("percobaan2.jpg")
+background.show()
